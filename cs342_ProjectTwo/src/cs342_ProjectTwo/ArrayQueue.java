73b8ed362ec4a23ea8dfc281ede1c2f898f1af3b
KngArthr <56852100+KngArthr@users.noreply.github.com>
2020-04-17 00:43:18 -0400

    erhjtj

--- cs342_ProjectTwo/src/cs342_ProjectTwo/ArrayQueue.java
+++ cs342_ProjectTwo/src/cs342_ProjectTwo/ArrayQueue.java
@@ -2,4 +2,107 @@ package cs342_ProjectTwo;
 
 public class ArrayQueue {
 
+	private static final int QUEUE_SIZE = 20;
+	private int[] queue;
+	private int count;
+	private int head;
+	private int tail;
+	
+	public ArrayQueue() {
+		clear();
+	}
+
+	public boolean add(int data) {
+		
+		if (isFull()) {
+			return false;
+		}		
+		
+		queue[tail] = data;
+		tail++;
+		if (tail == queue.length) {
+			tail = 0;
+		}
+		count++;
+		return true;
+	}
+
+	public int remove() {
+		//put error handling here
+		if (isEmpty()) {
+			return -1;
+		}
+		
+		int rtn = queue[head];
+		head++;
+		if (head == queue.length) {
+			head = 0;
+		}
+		count--;
+		
+		return rtn;
+	}
+
+	public void clear() {
+		queue = new int[QUEUE_SIZE];
+		count = 0;
+	}
+
+	public int size() {
+		return count;
+	}
+
+	public int maxCapacity() {
+		return queue.length;
+	}
+
+	public int peek() {
+		//put error handling
+		if (isEmpty()) {
+			
+			return -1;
+		}
+		
+		return queue[head];
+	}
+	
+	public boolean isEmpty() {
+		return count == 0;
+	}
+	
+	public boolean isFull() {
+		return count == queue.length;
+	}
+	
+
+	public String toString() {
+		String rtn = "";
+		
+		if (count == 0) {
+			return "<Empty>";
+		} else {
+			int n = head;
+			int c = count;
+			
+			while(c > 0) {
+				if (n == head) {
+					rtn += "head -> ";
+				} else {
+					rtn += "        ";
+				}
+				
+				rtn += queue[n] + "\n";
+				n++;
+				if (n == queue.length) {
+					n = 0;
+				}
+				
+				c--;
+			}
+			
+			return rtn;
+		}
+	}
+		
+	
 }
--- cs342_ProjectTwo/src/cs342_ProjectTwo/Driver.java
+++ cs342_ProjectTwo/src/cs342_ProjectTwo/Driver.java
@@ -13,6 +13,26 @@ public class Driver {
 		
 		userInterface.promptForNumber();
 		
+		ArrayQueue q = new ArrayQueue();
+		
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		q.add(4);
+		System.out.println(q);
+		
+		System.out.println("Item at head of queue is : " + q.peek());
+		System.out.println("Queue length is          : " + q.size());
+
+//		String r;
+//		while((r = q.remove()) != null) {
+//			System.out.println("Removed : " + r);
+		
+		while(q.size() > 0) {
+			System.out.println("Removing " + q.peek());
+			q.remove();
+		}
+		
 	}
 
 }
